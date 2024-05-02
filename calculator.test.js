e1239f8338cba5184af5caeffb84959e36fdcce6
jarguello1 <jcarguello11@live.com>
2024-05-02 11:16:52 -0400

    Add multiplication and division tests

--- calculator.test.js
+++ calculator.test.js
@@ -4,6 +4,14 @@ test('adds 1 + 2 to equal 3', () => {
   expect(calculator.add(1, 2)).toBe(3);
 });
 
-test('adds 3 - 2 to equal 1', () => {
-    expect(calculator.subtract(3, 2)).toBe(1);
-  });
+test('subtracts 3 - 2 to equal 1', () => {
+  expect(calculator.subtract(3, 2)).toBe(1);
+});
+
+test('multiply 10 * 15 to equal 150', () => {
+  expect(calculator.multiply(10, 15)).toBe(150)
+})
+
+test('divide 400 / 20 to equal 20', () => {
+  expect(calculator.divide(400, 20)).toBe(20)
+})
